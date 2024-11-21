1602c239ed14f6bab61dd6bdf3f0d320ac8f7562
tinker <fuazying@gmail.com>
2024-11-21 21:11:25 +0800

    架设起ktor server 的框架

---
+++ server/src/main/kotlin/app/penny/config/DatabaseFactory.kt
@@ -0,0 +1,29 @@
+package app.penny.config
+
+import app.penny.models.User
+import com.typesafe.config.ConfigFactory
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.transactions.transaction
+
+object DatabaseFactory {
+    fun init() {
+
+        val originalConfig = ConfigFactory.load()
+
+        val config = originalConfig.getConfig("database")
+
+        val driverClassName = config.getString("driver")
+
+        //local mysql
+        val jdbcURL = config.getString("url")
+        val user = config.getString("user")
+        val password = config.getString("password")
+
+        Database.connect(jdbcURL, driver = driverClassName, user = user, password = password)
+
+        transaction {
+            SchemaUtils.create(User)
+        }
+    }
+}
